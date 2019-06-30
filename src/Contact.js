31a2a64a71aa0ac4fc7e9b94b7d3dd49a19e83e1
Anson Melakayil <ansonmelakayil@Ansons-MacBook-Pro.local>
2019-06-29 20:03:28 -0400

    initial commit

---
+++ src/Contact.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Contact Me</h2>
+                <p>Money oer bitches all day err day</p>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+
+
+            </div>
+        );
+    }
+}
+
+export default Contact;
---
+++ src/Home.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Information</h2>
+                <p>insert info and background here</p>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+
+
+            </div>
+        );
+    }
+}
+
+export default Home;
